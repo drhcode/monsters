87a64f3f5e3d480c67199afa8c7a84e9058a9fda
drhcode <drhcode1@gmail.com>
2022-06-30 19:10:43 +0200

    Finished css and seach bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+
+// aead5d1d - Movie Api Key
+
+const API_URL = "http://www.omdbapi.com/?apikey=aead5d1d";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  });
+
   return (
-    <div>
-      <h1> Hello </h1>
+    <div className="app">
+      <h1> Movie Monsters</h1>
+      <div className="search">
+        <input
+          placeholder="Search any movie"
+          value="Avengers"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="Search" onClick={() => {}} />
+      </div>
     </div>
   );
 };
